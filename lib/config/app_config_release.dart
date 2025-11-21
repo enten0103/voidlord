53e8f375cc1659898ebe97730b173d13fb1844d1
enten0103 <enten@gmail.com>
2025-11-21 19:45:41 +0800

    fix(config):修正后端路径

--- lib/config/app_config_release.dart
+++ lib/config/app_config_release.dart
@@ -2,6 +2,6 @@ import 'package:voidlord/config/app_config.dart';
 
 // ignore: constant_identifier_names
 const AppConfig ReleaseConfig = AppConfig(
-  baseUrl: 'https://115.190.182.209:3000',
-  minioUrl: 'https://115.190.182.209:9000',
+  baseUrl: 'http://115.190.182.209:3000',
+  minioUrl: 'http://115.190.182.209:9000',
 );
